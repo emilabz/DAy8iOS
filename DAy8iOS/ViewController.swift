c258b0e88c639794e32c9851f2882b256b13b22a
MacStudent <macstudent@126-imac22.local>
2019-03-12 19:52:41 -0400

    plist created and read

--- DAy8iOS/ViewController.swift
+++ DAy8iOS/ViewController.swift
@@ -12,9 +12,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.readInformationPlist()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    func readInformationPlist() {
+        if let bundlePath = Bundle.main.path(forResource: "employee", ofType: "plist") {
+            let dictionary = NSMutableDictionary(contentsOfFile: bundlePath)
+            
+            print(dictionary!.description)
+            let countryList=dictionary!["countries"] as! NSArray
+            for i in countryList{
+                print(i)
+            }
+            print("**********************\nEmployee Details\n**********************\n////////////////////////////////")
+            let employeeList=dictionary!["employees"] as! NSArray
+            for e in employeeList{
+                let ed=e as! NSDictionary
+                print("Employee ID: \(ed["eid"]!)")
+                print("Employee name: \(ed["ename"]!)")
+                print("Salary: \(ed["salary"]!)")
+                print("////////////////////////////////")
+            }
+        }
+    }
 }
 
